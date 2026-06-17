2474ec72607396ef972cd65569b0c8d0484e222c
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-17 12:07:03 +0600

    Auto commit: 2026-06-17 12:07:03

--- orabooks-ui/src/pages/frontend/pages/NotificationsPage.tsx
+++ orabooks-ui/src/pages/frontend/pages/NotificationsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { api } from './api';
+import { api } from '../api';
 import { Bell, Mail, Smartphone, Monitor } from 'lucide-react';
 
 export default function NotificationsPage() {
