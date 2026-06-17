3bd7a965f120a985d8f8e861d0a7d318b63a2b50
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-17 11:05:30 +0600

    Auto commit: 2026-06-17 11:05:29

--- orabooks-ui/src/pages/admin/main.tsx
+++ orabooks-ui/src/pages/admin/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 import AdminRoutes from './routes';
-import './styles/index.css';
+import '@/styles/index.css';
 
 const router = createHashRouter([
   {
