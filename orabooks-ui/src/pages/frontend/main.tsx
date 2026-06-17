68385ab0ee680a86cecf2e45bff025614f6817e7
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-17 11:05:06 +0600

    Auto commit: 2026-06-17 11:05:06

--- orabooks-ui/src/pages/frontend/main.tsx
+++ orabooks-ui/src/pages/frontend/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 import FrontendRoutes from './App';
-import './styles/index.css';
+import '@/styles/index.css';
 
 const router = createHashRouter([
   {
