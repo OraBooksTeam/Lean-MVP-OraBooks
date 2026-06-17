db434668f9a6a3798e2d7da825552184b3d93ec8
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-17 12:37:47 +0600

    Auto commit: 2026-06-17 12:37:47

--- orabooks-ui/src/pages/frontend/pages/RegisterPage.tsx
+++ orabooks-ui/src/pages/frontend/pages/RegisterPage.tsx
@@ -1,7 +1,7 @@
 import { useState, type FormEvent } from 'react';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
-import { api } from './api';
+import { api } from '../api';
 import { UserPlus } from 'lucide-react';
 
 export default function RegisterPage() {
