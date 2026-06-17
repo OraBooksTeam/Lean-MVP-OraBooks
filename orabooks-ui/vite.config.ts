26d0798845566e154bf43bd56f8d1dd43fdc5bec
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-17 15:07:34 +0600

    Auto commit: 2026-06-17 15:07:34

--- orabooks-ui/vite.config.ts
+++ orabooks-ui/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   root: 'src',
   publicDir: '../public',
   build: {
-    outDir: '../../dist',
+    outDir: path.resolve(__dirname, '../OraBooks Lean MVP/assets/react'),
     emptyOutDir: true,
     rollupOptions: {
       input: {
