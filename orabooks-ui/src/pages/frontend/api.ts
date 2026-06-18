08e04782f989e9b61515de6fb0d852eb27d89d88
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-18 13:19:12 +0600

    Auto commit: 2026-06-18 13:19:12

--- orabooks-ui/src/pages/frontend/api.ts
+++ orabooks-ui/src/pages/frontend/api.ts
@@ -665,5 +665,7 @@ export const api = {
   securityVerifyControls: () =>
     api.post('orabooks_security_verify_controls'),
   exportsList: (page = 1) => api.get('orabooks_exports_list', { page }),
+  exportCancel: (exportId: number) => api.post('orabooks_export_cancel', { export_id: exportId }),
+  exportDownload: (exportId: number) => api.get('orabooks_export_download', { export_id: exportId }),
   csvImportsDashboard: () => api.get('orabooks_csv_imports_dashboard'),
 };
