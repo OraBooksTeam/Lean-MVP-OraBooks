93dcc6030b5b2c97f7975369709d0260009a6552
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-20 15:43:08 +0600

    Auto commit: 2026-06-20 15:43:08

---
+++ OraBooks Lean MVP/includes/jobs/class-orabooks-job-worker.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * SL-303 worker facade.
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class OraBooks_Job_Worker {
+    public static function process() {
+        return OraBooks_AsyncQueue::init()->process_queue();
+    }
+
+    public static function heartbeat_recovery() {
+        return OraBooks_AsyncQueue::init()->heartbeat_recovery();
+    }
+}
