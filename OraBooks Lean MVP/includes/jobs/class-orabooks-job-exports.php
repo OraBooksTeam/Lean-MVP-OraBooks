3e9514bb4cc4e57ca9564db33437bd9ba823daa9
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-20 15:43:22 +0600

    Auto commit: 2026-06-20 15:43:22

---
+++ OraBooks Lean MVP/includes/jobs/class-orabooks-job-exports.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * SL-303 report export helpers.
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class OraBooks_Job_Exports {
+    public static function enqueue_report($report_type, $parameters = [], $options = []) {
+        return OraBooks_AsyncQueue::enqueue('export_report_async', array_merge($parameters, [
+            'report_type' => sanitize_key($report_type),
+        ]), array_merge([
+            'queue_name' => 'reports',
+            'priority' => 4,
+            'max_retries' => 5,
+        ], $options));
+    }
+}
