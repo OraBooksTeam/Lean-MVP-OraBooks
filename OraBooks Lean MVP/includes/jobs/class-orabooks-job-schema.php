6c79f4a2ea5b6c26a26816367d993d7e36ea3336
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-20 15:43:01 +0600

    Auto commit: 2026-06-20 15:43:01

---
+++ OraBooks Lean MVP/includes/jobs/class-orabooks-job-schema.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * SL-303 schema facade.
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class OraBooks_Job_Schema {
+    public static function jobs_table() {
+        return OraBooks_Database::table(OraBooks_AsyncQueue::TABLE_JOBS);
+    }
+
+    public static function audit_table() {
+        return OraBooks_Database::table(OraBooks_AsyncQueue::TABLE_AUDIT);
+    }
+}
