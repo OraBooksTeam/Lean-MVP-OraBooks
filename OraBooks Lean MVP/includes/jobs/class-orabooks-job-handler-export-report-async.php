66100a41b2dcf6ab2c9b92d84aae913f31b91173
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-20 15:43:29 +0600

    Auto commit: 2026-06-20 15:43:29

---
+++ OraBooks Lean MVP/includes/jobs/class-orabooks-job-handler-export-report-async.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * SL-303 export_report_async handler wrapper.
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class OraBooks_Job_Handler_Export_Report_Async {
+    public static function handle($job, $payload) {
+        return OraBooks_AsyncQueue::handle_export_report_async($job, $payload);
+    }
+}
---
+++ OraBooks Lean MVP/includes/jobs/class-orabooks-job-handler-send-notification-email.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * SL-303 send_notification_email handler wrapper.
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class OraBooks_Job_Handler_Send_Notification_Email {
+    public static function handle($job, $payload) {
+        return OraBooks_AsyncQueue::handle_send_notification_email($job, $payload);
+    }
+}
