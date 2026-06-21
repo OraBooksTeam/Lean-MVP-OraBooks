4402ae4eb156612e7f5728e968b9b55608958b19
OraBooksTeam <taxorallc.github@gmail.com>
2026-06-21 10:21:15 +0600

    Auto commit: 2026-06-21 10:21:15

--- OraBooks Lean MVP/tests/bootstrap-secrets.php
+++ OraBooks Lean MVP/tests/bootstrap-secrets.php
@@ -4,4 +4,15 @@
  */
 
 $GLOBALS['ORABOOKS_LOAD_REAL_SECRETS'] = true;
+
+if (!defined('LOGGED_IN_KEY')) {
+    define('LOGGED_IN_KEY', 'test-logged-in-key-for-orabooks-secrets');
+}
+
 require __DIR__ . '/bootstrap.php';
+
+if (!function_exists('wp_salt')) {
+    function wp_salt($scheme = 'auth') {
+        return 'test-wp-salt-' . $scheme;
+    }
+}
