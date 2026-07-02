f746f0266d62c8a97ba12360a111e08c27eba83f
OraBooksTeam <taxorallc.github@gmail.com>
2026-07-02 11:28:31 +0600

    Auto commit: 2026-07-02 11:28:31

--- OraBooks Lean MVP/includes/helpers.php
+++ OraBooks Lean MVP/includes/helpers.php
@@ -2464,7 +2464,7 @@ function orabooks_resolve_auth_org_id($user_id, $stored_org_id = 0) {
     if ($user_id > 0) {
         $table_user_org = OraBooks_Database::table('user_org');
         $member_org_id = (int) $wpdb->get_var($wpdb->prepare(
-            "SELECT org_id FROM {$table_user_org} WHERE user_id = %d ORDER BY joined_at DESC, id DESC LIMIT 1",
+            "SELECT org_id FROM {$table_user_org} WHERE user_id = %d ORDER BY joined_at DESC LIMIT 1",
             $user_id
         ));
         if ($member_org_id > 0) {
